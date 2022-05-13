8135d2a79fff4035e32431b06b2466d04d873993
rchesa21 <radellachesas@gmail.com>
2022-05-13 10:34:52 +0700

    first commit

---
+++ app/Views/TesView.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document Title</title>
+</head>
+<body>
+    <table>
+        <tr>
+            <th>NO</th>
+            <th>NAMA</th>
+            <th>KELAS</th>
+        </tr>
+        <tr>
+            <th>1</th>
+            <th>ELZHA</th>
+            <th>XI RPL 2</th>
+        </tr>
+
+</body>
+</html>
